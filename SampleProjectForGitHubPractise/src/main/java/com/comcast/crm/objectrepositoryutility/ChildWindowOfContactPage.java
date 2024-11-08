d4bbe5b7d71e6967691e3697a5fa94533bbd370d
asranjith <rockyranjith27@gmail.com>
2024-11-08 16:01:46 +0530

    child window of contact is added

---
+++ SampleProjectForGitHubPractise/src/main/java/com/comcast/crm/objectrepositoryutility/ChildWindowOfContactPage.java
@@ -0,0 +1,11 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class ChildWindowOfContactPage {
+	public ChildWindowOfContactPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+}
